5ad0ff6f366594ce6933c705420a43f871628044
Agoth Arop <agoth.a3@gmail.com>
2025-08-15 22:19:49 -0600

    Refactored getNeighbours function and improve getKnightShortestPath logic

--- knightMoves.js
+++ knightMoves.js
@@ -1,32 +1,70 @@
-class Node {
-    constructor(row, col, distanceFromStartPosition) {
-        this.row = row;
-        this.col = col;
-        this.distanceFromStartPosition = distanceFromStartPosition;
-    }
+const directions = [
+  [-2, -1],
+  [-2, 1],
+  [-1, -2],
+  [-1, 2],
+  [2, -1],
+  [2, 1],
+  [1, -2],
+  [1, 2],
+];
 
+class Node {
+  constructor(row, col, distanceFromStartPosition) {
+    this.row = row;
+    this.col = col;
+    this.distanceFromStartPosition = distanceFromStartPosition;
+  }
+
+  get getPositionString() {
+    return `${this.row}, ${this.col}`;
+  }
 }
 
-const getNeighbours = (row, col) => {}
+const getNeighbours = (row, col) => {
+  const neighbours = [];
 
-function getKnightShortestPath(x, y) {
-    const queue = [];
-    const startNode = new Node(0, 0, 0);
-    queue.push(startNode);
+  for (const direction of directions) {
+    const [rowChange, colChange] = direction;
 
-    // Visited?
-     const visited = new Set();
-     while (queue.length > 0) {
-        // Remove node
-        // In practice we should use a real queue class so we can dequeue in O(1)
-        const node = queue.shift();
-        const { row, col, distanceFromStartPosition} = node;
+    const neighbourRow = row + rowChange;
+    const neighbourCol = col + colChange
 
-        // Process node
+    neighbours.push([neighbourRow, neighbourCol])
+  }
+  
+};
 
-        // Add neighbours
-        for (const neighbour of getNeighbours(row, col)) {
+function getKnightShortestPath(targetRow, targetCol) {
+  const queue = [];
+  const startNode = new Node(0, 0, 0);
+  queue.push(startNode);
 
-        }
-     }
+  // Visited?
+  const visited = new Set();
+  while (queue.length > 0) {
+    // Remove node
+    // In practice we should use a real queue class so we can dequeue in O(1)
+    const node = queue.shift();
+    const { row, col, distanceFromStartPosition } = node;
+
+    // Process node
+    if (row === targetRow && col === targetCol)
+      return distanceFromStartPosition;
+    visited.add(node.getPositionString());
+
+    // Add neighbours
+    for (const neighbour of getNeighbours(row, col)) {
+      const [neighbourRow, neighbourCol] = neighbour;
+      const neighbourNode = new Node(
+        neighbourRow,
+        neighbourCol,
+        distanceFromStartPosition + 1
+      );
+
+      if (visited.has(neighbourNode.getPositionString())) continue;
+
+      queue.push(neighbourNode);
+    }
+  }
 }
