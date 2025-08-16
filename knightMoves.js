1e46850d4c8ede4d8398d06edc99b2c78b3469c3
Agoth Arop <agoth.a3@gmail.com>
2025-08-15 22:09:21 -0600

    ImplementedgetNeighbours function and enhance getKnightShortestPath logic

--- knightMoves.js
+++ knightMoves.js
@@ -6,7 +6,27 @@ class Node {
     }
 
 }
+
+const getNeighbours = (row, col) => {}
+
 function getKnightShortestPath(x, y) {
     const queue = [];
     const startNode = new Node(0, 0, 0);
+    queue.push(startNode);
+
+    // Visited?
+     const visited = new Set();
+     while (queue.length > 0) {
+        // Remove node
+        // In practice we should use a real queue class so we can dequeue in O(1)
+        const node = queue.shift();
+        const { row, col, distanceFromStartPosition} = node;
+
+        // Process node
+
+        // Add neighbours
+        for (const neighbour of getNeighbours(row, col)) {
+
+        }
+     }
 }
